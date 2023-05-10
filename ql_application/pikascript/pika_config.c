c71749008917423309c24344fd7e1020840761c8
dreamcmi <1912544842@qq.com>
2023-05-10 19:05:03 +0800

    canonical pika_platform_xxx function

--- ql_application/pikascript/pika_config.c
+++ ql_application/pikascript/pika_config.c
@@ -6,76 +6,48 @@
 
 #include "pika_config.h"
 
-// int __platform_vsnprintf(char* buff,
-//                          size_t size,
-//                          const char* fmt,
-//                          va_list args) {
-//     return vsnprintf(buff, size, fmt, args);
-// }
-
-// int __platform_sprintf(char* buff, char* fmt, ...) {
-//     va_list args;
-//     va_start(args, fmt);
-//     int res = vsnprintf(buff, PIKA_SPRINTF_BUFF_SIZE, fmt, args);
-//     va_end(args);
-//     return res;
-// }
-
-int __platform_putchar(char ch) {
-    unsigned char str[1] = {0};
-    str[0] = ch;
-    ql_uart_write(QL_UART_PORT_2, str, 1);
+int pika_platform_putchar(char ch) {
+    unsigned char str[1] = {(unsigned char)ch};
+    return ql_uart_write(QL_UART_PORT_2, str, 1);
 }
 
-void __platform_sleep_ms(uint32_t ms) {
-    ql_rtos_task_sleep_ms((uint32_t)ms);
-}
-
-void __platform_sleep_s(uint32_t s) {
-    ql_rtos_task_sleep_s((uint32_t)s);
-}
-
-int64_t __platform_getTick(void) {
-    return (int64_t)ql_rtos_up_time_ms();
-}
-
-void* __platform_malloc(size_t size) {
+void* pika_platform_malloc(size_t size) {
     return os_malloc(size);
 }
 
-void* __platform_realloc(void* ptr, size_t size) {
+void* pika_platform_realloc(void* ptr, size_t size) {
     return os_realloc(ptr, size);
 }
 
-void* __platform_calloc(size_t num, size_t size) {
+void* pika_platform_calloc(size_t num, size_t size) {
     return os_calloc(num, size);
 }
 
-void __platform_free(void* ptr) {
+void pika_platform_free(void* ptr) {
     os_free(ptr);
 }
 
-void* __platform_memset(void* mem, int ch, size_t size) {
+void* pika_platform_memset(void* mem, int ch, size_t size) {
     return os_memset(mem, ch, size);
 }
 
-void* __platform_memcpy(void* dir, const void* src, size_t size) {
+void* pika_platform_memcpy(void* dir, const void* src, size_t size) {
     return os_memcpy(dir, src, size);
 }
 
-int __platform_memcmp(const void* s1, const void* s2, size_t n) {
+int pika_platform_memcmp(const void* s1, const void* s2, size_t n) {
     return os_memcmp(s1, s2, n);
 }
 
-void* __platform_memmove(void* s1, void* s2, size_t n) {
+void* pika_platform_memmove(void* s1, void* s2, size_t n) {
     return os_memmove(s1, s2, n);
 }
 
-void __platform_disable_irq_handle(void) {
+void pika_platform_disable_irq_handle(void) {
     ql_rtos_exit_critical();
 }
 
-void __platform_enable_irq_handle(void) {
+void pika_platform_enable_irq_handle(void) {
     ql_rtos_enter_critical();
 }
 
