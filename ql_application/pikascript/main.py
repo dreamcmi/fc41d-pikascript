87ca0309eb7364e6714b3e5eaad8aae16706d237
dreamcmi <1912544842@qq.com>
2023-05-11 13:51:11 +0800

    add _thread

--- ql_application/pikascript/main.py
+++ ql_application/pikascript/main.py
@@ -7,6 +7,7 @@ import hmac
 import base64
 import time
 import network
+import _thread
 
 mem = PikaStdLib.MemChecker()
 
@@ -27,33 +28,40 @@ def cb1(signal):
 
 G1.setCallBack(cb1, G1.SIGNAL_RISING)
 
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
-wlan.connect("AR300M-NOR", "goodlife")
-while wlan.isconnected() == 0:
-    pass
+def task1():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect("AR300M-NOR", "goodlife")
+    while wlan.isconnected() == 0:
+        time.sleep_ms(100)
+    wlan.ifconfig()
+    while True:
+        mem.now()
+        time.sleep_ms(1000)
 
+
+_thread.start_new_thread(task1, ())
+
+while 1:
+    time.sleep(1)
 
 #################
-print("socket test begin")
-print("当前占用内存:",mem.now())
-s = socket.socket()
-s.setblocking(False) 
-host = "112.125.89.8"
-port = 37803
-s.connect((host, port))
-print("连接成功占用内存:",mem.now())
-s.send("hello pika from fc41d")
-print("发送消息占用内存:",mem.now())
-m = s.recv(10)
-print(m)
-print("接收消息占用内存:",mem.now())
-s.close()
-print("关闭连接占用内存:",mem.now())
-del s
-print("清理现场占用内存:",mem.now())
+# print("socket test begin")
+# print("当前占用内存:",mem.now())
+# s = socket.socket()
+# s.setblocking(False) 
+# host = "112.125.89.8"
+# port = 37803
+# s.connect((host, port))
+# print("连接成功占用内存:",mem.now())
+# s.send("hello pika from fc41d")
+# print("发送消息占用内存:",mem.now())
+# m = s.recv(10)
+# print(m)
+# print("接收消息占用内存:",mem.now())
+# s.close()
+# print("关闭连接占用内存:",mem.now())
+# del s
+# print("清理现场占用内存:",mem.now())
 #################
 
-while True:
-    mem.now()
-    time.sleep_ms(1000)
